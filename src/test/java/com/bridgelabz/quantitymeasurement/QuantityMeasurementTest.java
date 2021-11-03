06a93d1e655d14ce900adc52b9a24197ad06ca7f
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-11-03 15:27:53 +0530

    Added: testcase having different values and same unit.

--- src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
@@ -11,4 +11,11 @@ public class QuantityMeasurementTest {
         Length length2 = new Length(Length.Unit.FEET, 0.0);
         Assert.assertEquals(length1, length2);
     }
+
+    @Test
+    public void whenGivenTwoDifferentFeetValueObject_shouldReturnFalse() {
+        Length length1 = new Length(Length.Unit.FEET, 0.0);
+        Length length2 = new Length(Length.Unit.FEET, 1.0);
+        Assert.assertNotEquals(length1, length2);
+    }
 }
