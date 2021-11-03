39d5932ccd4010e099049e6ce08c2790791fe1eb
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-11-03 15:23:01 +0530

    Added: code to fullfill the given test requirment

---
+++ src/main/java/com/bridgelabz/quantitymeasurement/Length.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.quantitymeasurement;
+
+import java.util.Objects;
+
+/**
+ * @author Hp
+ */
+public class Length {
+
+    enum Unit {FEET, INCH};
+
+    private final Unit unit;
+    private final double value;
+
+    public Length(Unit unit, double value) {
+        this.unit = unit;
+        this.value = value;
+    }
+
+    /**
+     * @param o -> object
+     * @return ->Returns boolean value by comparing objects. If the objects are
+     * same it returns true.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Length length = (Length) o;
+        return java.lang.Double.compare(length.value, value) == 0;
+    }
+}
