0d13b3e314260b303d7bbf79b6bc1691683d5eb8
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-11-04 15:23:30 +0530

    Added: Changes made in testcases

--- src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
@@ -7,59 +7,59 @@ public class QuantityMeasurementTest {
 
     @Test
     public void whenGivenTwoSameFeetValueObject_ifEqual_shouldReturnTrue() {
-        Length length1 = new Length(Length.Unit.FEET, 0.0);
-        Length length2 = new Length(Length.Unit.FEET, 0.0);
-        Assert.assertEquals(length1, length2);
+        UnitMeasurementSystem unitMeasurementSystem1 = new UnitMeasurementSystem(Length.FEET, 0.0);
+        UnitMeasurementSystem unitMeasurementSystem2 = new UnitMeasurementSystem(Length.FEET, 0.0);
+        Assert.assertEquals(unitMeasurementSystem1, unitMeasurementSystem2);
     }
 
     @Test
     public void whenGivenTwoDifferentFeetValueObject_shouldReturnFalse() {
-        Length length1 = new Length(Length.Unit.FEET, 0.0);
-        Length length2 = new Length(Length.Unit.FEET, 1.0);
-        Assert.assertNotEquals(length1, length2);
+        UnitMeasurementSystem unitMeasurementSystem1 = new UnitMeasurementSystem(Length.FEET, 0.0);
+        UnitMeasurementSystem unitMeasurementSystem2 = new UnitMeasurementSystem(Length.FEET, 1.0);
+        Assert.assertNotEquals(unitMeasurementSystem1, unitMeasurementSystem2);
     }
 
     @Test
     public void whenGivenTwoSameInchValueObjects_shouldReturnTrue() {
-        Length length1 = new Length(Length.Unit.INCH, 0.0);
-        Length length2 = new Length(Length.Unit.INCH, 0.0);
-        Assert.assertEquals(length1, length2);
+        UnitMeasurementSystem unitMeasurementSystem1 = new UnitMeasurementSystem(Length.INCH, 0.0);
+        UnitMeasurementSystem unitMeasurementSystem2 = new UnitMeasurementSystem(Length.INCH, 0.0);
+        Assert.assertEquals(unitMeasurementSystem1, unitMeasurementSystem2);
     }
 
     @Test
     public void whenGivenTwoDifferentInchObjects_shouldReturnFalse() {
-        Length length1 = new Length(Length.Unit.INCH, 0.0);
-        Length length2 = new Length(Length.Unit.INCH, 1.0);
-        Assert.assertNotEquals(length1, length2);
+        UnitMeasurementSystem unitMeasurementSystem1 = new UnitMeasurementSystem(Length.INCH, 0.0);
+        UnitMeasurementSystem unitMeasurementSystem2 = new UnitMeasurementSystem(Length.INCH, 1.0);
+        Assert.assertNotEquals(unitMeasurementSystem1, unitMeasurementSystem2);
     }
 
     @Test
     public void whenGiven1FeetAnd1Feet_shouldReturnEquals() {
-        Length length1 = new Length(Length.Unit.FEET, 1.0);
-        Length length2 = new Length(Length.Unit.FEET, 1.0);
-        Assert.assertEquals(length1, length2);
+        UnitMeasurementSystem unitMeasurementSystem1 = new UnitMeasurementSystem(Length.FEET, 1.0);
+        UnitMeasurementSystem unitMeasurementSystem2 = new UnitMeasurementSystem(Length.FEET, 1.0);
+        Assert.assertEquals(unitMeasurementSystem1, unitMeasurementSystem2);
     }
 
     @Test
     public void whenGiven0FeetAnd0Inch_shouldReturnEquals() {
-        Length length1 = new Length(Length.Unit.FEET, 0.0);
-        Length length2 = new Length(Length.Unit.INCH, 0.0);
-        Assert.assertEquals(length1, length2);
+        UnitMeasurementSystem unitMeasurementSystem1 = new UnitMeasurementSystem(Length.FEET, 0.0);
+        UnitMeasurementSystem unitMeasurementSystem2 = new UnitMeasurementSystem(Length.INCH, 0.0);
+        Assert.assertEquals(unitMeasurementSystem1, unitMeasurementSystem2);
     }
 
     @Test
     public void whenGivenFeetAndInchObjects_shouldNotEquals() {
-        Length length1 = new Length(Length.Unit.FEET, 1.0);
-        Length length2 = new Length(Length.Unit.INCH, 1.0);
-        length1.compare(length2);
-        Assert.assertNotEquals(length1, length2);
+        UnitMeasurementSystem unitMeasurementSystem1 = new UnitMeasurementSystem(Length.FEET, 1.0);
+        UnitMeasurementSystem unitMeasurementSystem2 = new UnitMeasurementSystem(Length.INCH, 1.0);
+        unitMeasurementSystem1.compare(unitMeasurementSystem2);
+        Assert.assertNotEquals(unitMeasurementSystem1, unitMeasurementSystem2);
     }
 
     @Test
     public void whenGivenFeetAnd12InchObjects_shouldTrue() {
-        Length length1 = new Length(Length.Unit.FEET, 1.0);
-        Length length2 = new Length(Length.Unit.INCH, 12.0);
-        boolean compareCheck = length1.compare(length2);
+        UnitMeasurementSystem unitMeasurementSystem1 = new UnitMeasurementSystem(Length.FEET, 1.0);
+        UnitMeasurementSystem unitMeasurementSystem2 = new UnitMeasurementSystem(Length.INCH, 12.0);
+        boolean compareCheck = unitMeasurementSystem1.compare(unitMeasurementSystem2);
         Assert.assertTrue(compareCheck);
     }
 }
