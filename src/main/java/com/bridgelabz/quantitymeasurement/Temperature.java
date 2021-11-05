c49ba5857e4ce9ebed94b2a0cde0e6b64b5c44e7
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-11-05 12:00:19 +0530

    Added: code for different temperature entites

--- src/main/java/com/bridgelabz/quantitymeasurement/Temperature.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/Temperature.java
@@ -1,4 +1,36 @@
 package com.bridgelabz.quantitymeasurement;
 
-public enum Temperature implements M{
+import java.util.function.Function;
+
+public enum Temperature implements MeasurementUnits {
+    FAHRENHEIT(true), CELCIUS(false);
+
+    final Function<Double, Double> degFToCelcius = (Double degF) -> (degF.doubleValue() - 32) * 5 / 9;
+    final Function<Double, Double> degCToCelcius = (Double degC) -> degC.doubleValue();
+
+    final Function<Double, Double> convertionValue;
+
+    private Temperature(Boolean isFahrenhiet) {
+        if (isFahrenhiet)
+            this.convertionValue = degFToCelcius;
+        else
+            this.convertionValue = degCToCelcius;
+    }
+
+    /**
+     * @param value-> getting value
+     * @return -> convert to base unit and return
+     */
+    @Override
+    public double convertToBaseUnit(double value) {
+        return convertionValue.apply(value);
+    }
+
+    /**
+     * @return-> Returns that it support addition
+     */
+    @Override
+    public boolean supportAddition() {
+        return false;
+    }
 }
